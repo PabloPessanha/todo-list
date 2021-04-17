2fe67acb6beb2be0c4d1542a4028e09c8d73f884
Pablo <pablopessanh@gmail>
2021-04-16 23:09:32 -0300

    receving new todo with input + button; added rules

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: ['test1', 'test2'],
+      newTodo: '',
+      todos: [],
     };
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ newTodo: target.value });
+  }
+
+  addTodo() {
+    const { todos, newTodo } = this.state;
+    if (!todos.some((todo) => todo === newTodo)) {
+      this.setState({ todos: [...todos, newTodo] }, () => {
+        this.setState({ newTodo: '' });
+      });
+    } else {
+      alert('Repeated todo not allowed!');
+      this.setState({ newTodo: '' });
+    }
   }
 
   render() {
-    const { todos } = this.state;
+    const { todos, newTodo } = this.state;
     return (
       <>
+        <input type="text" onChange={(e) => this.handleChange(e)} value={newTodo} />
+        <button type="button" onClick={this.addTodo}>Adicionar tarefa</button>
         <ul>
           {todos.map((todo) => <li key={todo}>{todo}</li>)}
         </ul>
